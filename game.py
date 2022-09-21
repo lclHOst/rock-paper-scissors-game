880c24723b02e468cf139d1fdb69d787e17cfc98
lclHOst <114050717+lclHOst@users.noreply.github.com>
2022-09-21 09:41:40 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,8 +1,15 @@
-human_turn = 'paper'
-computer_turn = 'scissors'
+import random 
+Turns = ['rock', 'paper', 'scissors' ] 
 
+
+
+human_turn = input("Enter your turn: ")
+computer_turn = random.choice(Turns)
+
+
+print(f'Human:{human_turn} vs. Computer:{computer_turn}')
 if human_turn == computer_turn:
-    Print('Draw!')
+    print('Draw!')
 elif human_turn == 'rock' and computer_turn == 'scissors':
     print('Human wins!')
 elif human_turn == 'paper' and computer_turn == 'rock':
